eb3533a46ef7fa15ba7be358fe417d096bf65edb
leochn <appchn@163.com>
2017-05-22 12:24:44 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <NavHeader></NavHeader>
+  </div>
+</template>
+
+<script>
+import NavHeader from './components/Nav.vue';
+
+export default {
+  name: 'app',
+  components:{
+    NavHeader
+  }
+}
+</script>
+
+<style>
+  
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,10 @@
+<template>
+	<div>
+		<ul>
+			<!-- <route-link to='/home' tag='li' active-class='active'>
+				<a href="javascript:;">首页</a>
+			</route-link> -->
+			首页
+		</ul>
+	</div>
+</template>
